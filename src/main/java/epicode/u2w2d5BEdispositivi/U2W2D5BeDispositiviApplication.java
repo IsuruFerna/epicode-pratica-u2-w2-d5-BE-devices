32d720e3fcead311f88acb65314fb94723a82a78
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-01-19 10:01:27 +0100

    first commit

---
+++ src/main/java/epicode/u2w2d5BEdispositivi/U2W2D5BeDispositiviApplication.java
@@ -0,0 +1,13 @@
+package epicode.u2w2d5BEdispositivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W2D5BeDispositiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W2D5BeDispositiviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/epicode/u2w2d5BEdispositivi/U2W2D5BeDispositiviApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.u2w2d5BEdispositivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W2D5BeDispositiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
